34d66913b153752a95c57dce7c98428b5d716388
harish-revature <harish.p.c@revature.com>
2026-02-04 14:30:17 +0530

    Backend features: admin roles, cart, favorites, product fixes

--- Backend/server.py
+++ Backend/server.py
@@ -1,6 +1,5 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
-
-
+    # HOST MUST BE 0.0.0.0 for Docker/Cloud
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
